7580c0ed9ed7a28b771eef743cb811537e190e80
Adson Gomes Oliveira <adsongoliveira2022@outlook.com>
2023-04-22 19:56:18 -0300

    <fix> Fix connection error

--- src/server.js
+++ src/server.js
@@ -8,10 +8,11 @@ const PORT = process.env.PORT || 3001;
 
 const USER = process.env.DB_USER || 'root';
 const PASSWORD = process.env.DB_PASSWORD || 'secret';
-const HOST = process.env.DB_HOST || '127.0.0.1:27017';
-const DATABASE = process.env.DB_DATABASE || 'ecomm-product?authSource=admin';
+const HOST = process.env.DB_HOST || 'mongodb-ms-product:27017';
+const DATABASE = process.env.DB_DATABASE || 'ms-product?authSource=admin';
+const MONGO_INITIAL = process.env.MONGO_INITIAL || 'mongodb';
 
-mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@${HOST}/${DATABASE}`)
+mongoose.connect(`${MONGO_INITIAL}://${USER}:${PASSWORD}@${HOST}/${DATABASE}`)
   .then(() => {})
   .catch((error) => console.error(error));
 
